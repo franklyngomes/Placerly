87eba501ccfecf25209932ceab1e5da4681a5c98
franklyngomes <franklyngomes15@gmail.com>
2025-08-29 11:16:35 +0530

    Attempt to fix CORS error

--- server/server.js
+++ server/server.js
@@ -1,23 +1,32 @@
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
-const dotenv = require('dotenv').config()
-const path = require("path")
-const flash = require('connect-flash')
-const session = require('express-session')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const dotenv = require("dotenv").config();
+const path = require("path");
+const flash = require("connect-flash");
+const session = require("express-session");
+const cors = require("cors");
 const MongoStore = require("connect-mongo");
 
-const DatabaseConnection = require("./app/config/dbCon")
-DatabaseConnection()
-app.set('view engine', 'ejs')
-app.set('views', 'views')
-app.use(express.json())
+const DatabaseConnection = require("./app/config/dbCon");
+DatabaseConnection();
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: "https://placerly-1.onrender.com", 
-  credentials: true
-}))
+
+app.use(
+  cors(
+    {
+      origin: "https://placerly-1.onrender.com",
+      credentials: true,
+    },
+    {
+      origin: "https://placerly.vercel.app/",
+      credentials: true,
+    }
+  )
+);
 
 app.use(
   session({
@@ -26,34 +35,33 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({
       mongoUrl: process.env.MONGODB_CONNECTION_STRING,
-      collectionName: "sessions"
+      collectionName: "sessions",
     }),
     cookie: {
       maxAge: 1000 * 60 * 60, // 1 hour
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production"
-    }
+      secure: process.env.NODE_ENV === "production",
+    },
   })
 );
-app.use(flash())
+app.use(flash());
 app.use((req, res, next) => {
   res.locals.messages = req.flash("message");
   next();
 });
 
-app.use(express.static(__dirname+ "/public"))
+app.use(express.static(__dirname + "/public"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //Admin Routes
-const adminRouter = require("./app/routes/AdminRoutes")
-app.use(adminRouter)
+const adminRouter = require("./app/routes/AdminRoutes");
+app.use(adminRouter);
 
-//Placerly Routes 
-const placerlyRouter = require("./app/routes/PlacerlyRoutes")
-app.use("/api", placerlyRouter)
+//Placerly Routes
+const placerlyRouter = require("./app/routes/PlacerlyRoutes");
+app.use("/api", placerlyRouter);
 
-
-const port = process.env.PORT
+const port = process.env.PORT;
 app.listen(port, () => {
-  console.log("Server is running on http://localhost:5000")
-})
+  console.log("Server is running on http://localhost:5000");
+});
