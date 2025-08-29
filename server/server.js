842f04534a54b714541f6e5cf92676862cc1cb71
franklyngomes <franklyngomes15@gmail.com>
2025-08-29 11:19:19 +0530

    Attempt to fix CORS error

--- server/server.js
+++ server/server.js
@@ -16,16 +16,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-  cors(
-    {
-      origin: "https://placerly-1.onrender.com",
-      credentials: true,
-    },
-    {
-      origin: "https://placerly.vercel.app/",
-      credentials: true,
-    }
-  )
+  cors({
+    origin: "https://placerly-1.onrender.com",
+    credentials: true,
+  })
+);
+app.use(
+  cors({
+    origin: "https://placerly.vercel.app/",
+    credentials: true,
+  })
 );
 
 app.use(
